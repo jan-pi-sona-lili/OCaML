72fc8a79e58f2a77f62e83866098430b1c31105e
ilo Kali <77077755+jan-pi-sona-lili@users.noreply.github.com>
2023-02-18 17:13:26 -0500

    Update main.ml
    need to add comments

--- main.ml
+++ main.ml
@@ -1,49 +1,76 @@
 (** OCaML - Machine Learning with OCaml. Uses the {{: https://nn.cs.utexas.edu/downloads/papers/stanley.ec02.pdf} NEAT } algorithm. *)
 
-type connection = {
-                enabled  : bool;
-                out_node : int;
-                weight   : float;
-                innov    : int; (** innovation number - see section 3.2 in the paper for details. *)
-} (** connection - describes a connection. note that it is slightly different compared to the paper's connection description, [{in_node:int; out_node:int; weight:float; enabled:bool; innovation:int}]. this is because the in_node is stored in its containing {{!type:node} node}, which reduces redundancy. *)
+module Network_parameter_types = struct
+        type threshold_weights =
+                { excess       : float
+                ; disjoint     : float
+                ; network_diff : float
+                }
+        (** Coefficients for adjusting how strongly a given factor affects the speciation threshold. *)
 
-type kind       = Sensor | Hidden | Output
-type node       = {
-                connections : (int, connection) Hashtbl.t;
-                kind        : kind;
-}
-(** Node - contains {{!type:connection} connections}. Note that it is slightly different compared to the paper's specification, [(int,kind)] (where [int] is the ID of the node). This is because the ID of the node is stored in the {{!type:genome} genome} itself, which reduces redundancy. *)
+        type network_dimensions =
+                { sensors : int
+                ; outputs : int
+                }
+        (** Size of the network's input and output. *)
 
-type genome     = {
-                sensors : node array;
-                hiddens : (int, node) Hashtbl.t;
-                outputs : node array;
-} (** Genome - contains all the nodes of a network. [sensors] and [outputs] are arrays (fixed size, mutable elements), as their size is known at network creation time, while [hiddens] must be able to grow (and is thus a hashtable). *)
+        type mutation_rates =
+                { add_node          : float
+                ; add_connection    : float
+                ; modify_connection : float
+                }
+        (** How often a given mutation happens. *)
+end 
 
-type 'a network = {
-                genome : genome;
-                mutable innovation : int; (** innovation number - see section 3.2 in the paper for details. *)
+module type Userdata = sig
+        type t
+        val fitness : t -> float
+end
 
-                mutable userdata : 'a array; (** holds user-specific information about a network. *)
-} (** Network - contains network-specific information. [userdata] contains network-specific user values (for example, position). *)
+module Model : sig
+        type t
+        val make :
+                ?activation        : (float -> float) ->
+                threshold_weights  : Network_parameter_types.threshold_weights ->
+                network_dimensions : Network_parameter_types.network_dimensions ->
+                mutation_rates     : Network_parameter_types.mutation_rates ->
+                network_count      : int ->
+                userdata           : (module Userdata) -> 
+                unit -> 
+                t
+end = struct 
+        module type IN = sig
+                include Userdata
+                val activation         : float -> float 
+                val threshold_weights  : Network_parameter_types.threshold_weights
+                val network_dimensions : Network_parameter_types.network_dimensions
+                val mutation_rates     : Network_parameter_types.mutation_rates
+                val network_count      : int 
+                val fitness            : t -> float
+        end
+        module type OUT = sig
+        end
 
-type 'a parameters = {
-        fitness_function     : 'a network -> int; (** fitness function - takes a network and returns its fitness. *)
-        input_function       : 'a network -> int list; (** input function - returns a list that is mapped onto the {{!field:genome.sensors} sensor nodes} of a network's genome. *)
-        activation_function  : float -> float; (** activation function - see {: https://en.wikipedia.org/wiki/Activation_function}. *)
+        type t = (module OUT)
 
-        weight_mutation_rate     : float;
-        connection_mutation_rate : float;
-        node_mutation_rate       : float;
+        module ModelF(M : IN) : OUT = M
 
-        coefficient1 : float; (** these three coefficients adjust the weights in the network distance function {i delta}. See section 3.3 in the paper for details. *)
-        coefficient2 : float;
-        coefficient3 : float;
-
-        default_userdata : 'a list; (** new networks' {{!field:network.userdata} userdata} are initialized with this. *)
-} (** Parameters - describes various parameters of the model. *)
-
-type 'a model   = {
-                networks   : 'a network array;
-                parameters : 'a parameters;
-} (** Model - contains all information about the model, including networks and parameters. *)
+        let make
+                ?(activation=fun x -> Float.(1. /. (1. +. (exp @@ neg x))))
+                ~threshold_weights
+                ~network_dimensions
+                ~mutation_rates
+                ~network_count
+                ~userdata
+                ()
+        =
+                let module M = ModelF(struct 
+                        include (val userdata : Userdata)
+                        let activation = activation
+                        let threshold_weights = threshold_weights
+                        let network_dimensions = network_dimensions
+                        let mutation_rates = mutation_rates
+                        let network_count = network_count
+                        let fitness = fitness
+                end) in (module M : OUT)
+end
